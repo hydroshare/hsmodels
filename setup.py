aae54f82dd42661654a2108f98155a7d1884f7a2
Scott Black <sblack@cuahsi.org>
2021-11-03 15:03:24 -0700

    increment patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ README = 'Refer to the models seciont of https://hydroshare.github.io/hsclient/'
 
 setup(
     name='hsmodels',
-    version='0.3.0a',
+    version='0.3.1',
     packages=find_packages(include=['hsmodels', 'hsmodels.*', 'hsmodels.schemas.*', 'hsmodels.schemas.rdf.*'],
                            exclude=("tests",)),
     install_requires=[

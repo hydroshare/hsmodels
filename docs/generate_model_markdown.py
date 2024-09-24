78721f7e5ae061e41366093b06ce4ae52afec619
pkdash <pkdash_reena@hotmail.com>
2024-09-24 10:38:34 -0400

    [#49] updating script to generate md file for CSV content type

--- docs/generate_model_markdown.py
+++ docs/generate_model_markdown.py
@@ -4,26 +4,33 @@ import json
 
 from hsmodels.schemas.resource import ResourceMetadata
 from hsmodels.schemas.aggregations import FileSetMetadata, GeographicRasterMetadata, GeographicFeatureMetadata, \
-    MultidimensionalMetadata, ReferencedTimeSeriesMetadata, SingleFileMetadata, TimeSeriesMetadata
+    MultidimensionalMetadata, ReferencedTimeSeriesMetadata, SingleFileMetadata, TimeSeriesMetadata, CSVFileMetadata
+
+aggregation_models = [
+    ResourceMetadata,
+    FileSetMetadata,
+    GeographicRasterMetadata,
+    GeographicFeatureMetadata,
+    MultidimensionalMetadata,
+    ReferencedTimeSeriesMetadata,
+    SingleFileMetadata,
+    TimeSeriesMetadata,
+    CSVFileMetadata
+]
 
 
 def write_md(model):
-    sj_rm = model.schema_json()
+    sj_rm = model.schema_json(indent=4)
+    sj_rm = sj_rm.replace("$defs", "definitions")
     parser = jsonschema2md.Parser()
     parser.tab_size = 4
     md_lines = parser.parse_schema(json.loads(sj_rm))
 
     filename = model.__name__ + ".md"
-    with open(os.path.join("docs", filename), "w") as f:
+    with open(os.path.join(filename), "w") as f:
         f.writelines(md_lines)
 
 
-write_md(ResourceMetadata)
-write_md(FileSetMetadata)
-write_md(GeographicRasterMetadata)
-write_md(GeographicFeatureMetadata)
-write_md(MultidimensionalMetadata)
-write_md(ReferencedTimeSeriesMetadata)
-write_md(SingleFileMetadata)
-write_md(TimeSeriesMetadata)
+for aggr_model in aggregation_models:
+    write_md(aggr_model)
 

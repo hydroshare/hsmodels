b76ab20bf6581f7f0c3e8e3cd713d5c21b4fa052
pkdash <pabitra.dash@usu.edu>
2026-06-18 22:01:30 -0400

    fix: update Python version classifiers in setup.py

--- setup.py
+++ setup.py
@@ -21,11 +21,15 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
 
 )
